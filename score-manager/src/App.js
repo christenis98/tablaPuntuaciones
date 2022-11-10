a4447d5ad30121cdd0b576cfcd9059de3c10edda
christenis98 <canei01@ucm.es>
2022-11-10 10:30:59 +0100

    Titulo solo

--- score-manager/src/App.js
+++ score-manager/src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import MainView from "./MainView.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+        <MainView />
   );
 }
 
---
+++ score-manager/src/MainView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function mainView()  {
+    return (
+        <div>
+            <h1>SOLERA TEAMS. BOOTCAMP 4</h1>
+        </div>
+    );
+}
+
+export default mainView;
