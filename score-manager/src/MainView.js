fe753c16642b4fbe8020ace98aee0f96cc369262
PedroCfF <pedrocienfuegos@gmail.com>
2022-11-10 12:25:36 +0100

    Update MainView.js

--- score-manager/src/MainView.js
+++ score-manager/src/MainView.js
@@ -1,11 +1,14 @@
 import React,{useState} from "react";
 import Score from "./components/Score";
 
-const mainView = (props) => {
-    // const [view,setView]=useState('');
+const mainView = ({scoreData}) => {
+    for(let i=0;i<scoreData.legth;i++)
+    {
+        console.log(scoreData[i]);
+    }
 
     return (
-        <div view={props.view}>
+        <div>
             <h1>SOLERA TEAMS. BOOTCAMP 4</h1>
             <div>
                 <Score />
