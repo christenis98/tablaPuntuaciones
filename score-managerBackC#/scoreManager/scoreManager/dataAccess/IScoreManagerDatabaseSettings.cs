f3fd49bb06831bd884cd1203b3c870da42790e52
claudy411 <86935807+claudy411@users.noreply.github.com>
2022-11-17 10:26:07 +0100

    Deleted scoreManager

--- score-managerBackC#/scoreManager/scoreManager/dataAccess/IScoreManagerDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace scoreManager.dataAccess
-{
-    public interface IScoreManagerDatabaseSettings
-    {
-        public string CollectionName { get; set; }
-        public string DatabaseName { get; set; }
-        public string ConnectionString { get; set; }
-    }
-}
