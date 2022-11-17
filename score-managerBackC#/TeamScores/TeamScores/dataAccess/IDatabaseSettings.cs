afe056ffc059b54b4339a6a6e68868aaa86b9476
claudy411 <86935807+claudy411@users.noreply.github.com>
2022-11-17 10:25:11 +0100

    Deleted TeamScores

--- score-managerBackC#/TeamScores/TeamScores/dataAccess/IDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TeamScores.dataAccess
-{
-    public interface IDatabaseSettings
-    {
-        public string CollectionName { get; set; }
-        public string DatabaseName { get; set; }
-        public string ConnectionString { get; set; }
-    }
-}
--- score-managerBackC#/TeamScores/TeamScores/model/Score.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TeamScores.model
-{
-    public class Score
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-        public int points { get; set; }
-    }
-}
