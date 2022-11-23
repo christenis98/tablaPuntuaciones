f181c07a90b8a14b2e5b15ce25bbe336f175e8e7
PedroCfF <pedrocienfuegos@gmail.com>
2022-11-23 14:31:10 +0100

    teams refreshing properly and team name done

--- score-manager/src/components/js/NewTeamButton.js
+++ score-manager/src/components/js/NewTeamButton.js
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import "../css/NewTeamButon.css";
 import { v4 as uuidv4 } from "uuid";
+import { useNavigate } from "react-router-dom";
 
 const NewTeam = (props) => {
   const number = props.teamcount;
   const API_URL = "http://localhost:8080/api/teams";
+  let teamnumber;
+
+  const[currentTeam, setCurrentTeam]=useState({})
+
+  if (props.teamnumber>0)
+  {
+    teamnumber=props.teamnumber
+  }
+  else{
+    teamnumber=1
+  }
 
   const newTeam = {
     id: uuidv4(),
-    name: "Team " + number,
+    name: "Team " + teamnumber,
     scores: [{ name: "task 1", points: 5 }],
   };
 
@@ -19,10 +31,15 @@ const NewTeam = (props) => {
       headers: {
         "Content-Type": "application/json",
       },
-    });
-    const fetchedData = await response;
-    const fetchedDataJson = await fetchedData.json();
-    console.log("Success: ", fetchedDataJson);
+    })
+
+    // const fetchedData = await response;
+    // const fetchedDataJson = await fetchedData.json();
+    // console.log("Success: ", fetchedDataJson);
+
+    setCurrentTeam(newTeam);
+    props.isclicked(true)
+
   };
 
   return (
